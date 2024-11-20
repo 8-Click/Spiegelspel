07bbca99705b5f72fd70e9f98b55b343db6704e1
Rick <144334803+8-Click@users.noreply.github.com>
2024-11-20 13:18:46 +0100

    homepagina af

--- public/info.php
+++ public/info.php
@@ -3,29 +3,75 @@ include_once("core/html.php");
 ?>
 <link rel="stylesheet" href="<?= BASEURL_PUBLIC ?>assets/css/info.css"><?php
   include_once("core/header.php"); ?>
-<div class="infoContainer">
-    <div class="videoContainer">
-        <div class="video">
-            <img id="videoImage" src="<?= BASEURL_PUBLIC ?>assets/img/video.png">
-            <p>Meerwaarde</p>
+<div class="parentContainer">
+    <div class="infoContainer">
+        <div class="videoContainer">
+            <div class="video">
+                <img id="videoImage" src="<?= BASEURL_PUBLIC ?>assets/img/video.png">
+                <p>Meerwaarde</p>
+            </div>
+            <div class="videoText">
+                <h1>DOEL SPIEGELSPEL</h1>
+                <p>Een methodiek om spelenderwijs het teamleren te bevorderen</p>
+            </div>
         </div>
-        <div class="videoText">
-            <h1>DOEL SPIEGELSPEL</h1>
-            <p>Een methodiek om spelenderwijs het teamleren te bevorderen</p>
+        <div class="fotoContainer">
+            <div class="fotoBox">
+                <img class="foto" src="<?= BASEURL_PUBLIC ?>assets/img/foto1.png">
+                <p>Kwaliteit</p>
+            </div>
+            <div class="fotoBox">
+                <img class="foto" src="<?= BASEURL_PUBLIC ?>assets/img/foto2.png">
+                <p>Samenwerken</p>
+            </div>
+            <div class="fotoBox">
+                <img class="foto" src="<?= BASEURL_PUBLIC ?>assets/img/foto2.png">
+                <p>Teamontwikkeling</p>
+            </div>
         </div>
     </div>
-    <div class="fotoContainer">
-        <div class="fotoBox">
-            <img class="foto" src="<?= BASEURL_PUBLIC ?>assets/img/foto1.png">
-            <p>Kwaliteit</p>
-        </div>
-        <div class="fotoBox">
-            <img class="foto" src="<?= BASEURL_PUBLIC ?>assets/img/foto2.png">
-            <p>Samenwerken</p>
-        </div>
-        <div class="fotoBox">
-            <img class="foto" src="<?= BASEURL_PUBLIC ?>assets/img/foto2.png">
-            <p>Teamontwikkeling</p>
+    <div class="instructionContainer">
+        <h2>HET SPIEGELSPEL</h2>
+        <div class="initialBox">
+            <div class="leftColumn">
+                <div class="paragraphImage">
+                    <img class="image" src="<?= BASEURL_PUBLIC ?>assets/img/infofoto1.png">
+                </div>
+                <div class="paragraph">
+                Een externe of intern opgeleide spelleider begeleidt het spel, waardoor de teammanager en het team
+                    zich
+                    volledig kunnen richten op hun werk zonder zich zorgen te maken over de methodiek of het proces.
+                    Vertrouwen in het vakmanschap van de professional staat centraal, waarbij alle teamleden actief
+                    deelnemen en bijdragen aan de teamontwikkeling.
+                </div>
+                <div class="paragraphImage">
+                    <img class="image" src="<?= BASEURL_PUBLIC ?>assets/img/infofoto3.png">
+                </div>
+            </div>
+            <div class="rightColumn">
+                <div class="paragraph">
+                Ontdek het krachtige Spiegelspel: een dynamische methode om samen als team de kwaliteit, prestaties
+                    en
+                    ontwikkeling te bespreken. In dit spel maakt het team gezamenlijk een waardevolle 'foto' van hun
+                    functioneren en identificeert gebieden voor groei. Het spel creëert een plezierige en veilige sfeer,
+                    waarin teamleden worden uitgenodigd om zich vrijelijk uit te spreken en deel te nemen.
+                    Het bewezen succes van het Spiegelspel wordt erkend door teamleden en teammanagers. Het omvat alle
+                    essentiële onderwerpen voor teamleren, met aandacht voor de behoeften van een Highperformance team
+                    en de
+                    ISO-/HKZ-normen. Het Spiegelspel biedt een interne audit en systeembeoordeling in één.
+                </div>
+                <div class="paragraphImage">
+                    <img class="image" src="<?= BASEURL_PUBLIC ?>assets/img/infofoto2.png">
+                </div>
+                <div class="paragraph">
+                    Tijdens het Spiegelspel ontstaat vanzelf interactie in het team, resulterend in waardevolle dialogen
+                    en
+                    zelfreflectie. De uitkomsten worden vervolgens vastgelegd in een concreet zelfevaluatierapport met
+                    sterke punten en ontwikkelpunten van het team. Dit rapport wordt overhandigd aan de teammanager en
+                    vormt
+                    een solide basis voor het opstellen van een effectief teamplan.
+                </div>
+            </div>
         </div>
     </div>
 </div>
