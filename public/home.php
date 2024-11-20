e00a1ef0d4f71807583fdcfeb20bed6826c2ad3c
Rick <144334803+8-Click@users.noreply.github.com>
2024-11-20 09:54:32 +0100

    Homepagina af, bezig met infopagina

--- public/home.php
+++ public/home.php
@@ -1,14 +1,13 @@
-<?php 
+<?php
 include_once("core/html.php");
-?><link rel="stylesheet" href="<?=BASEURL_PUBLIC?>assets/css/home.css"><?php
-include_once("core/header.php");?>
-
-
-<div class="homeContainer">
-    <div class="backgroundImage">
-        <div class="textBox">
-            <h1>SPIEGELSPEL</h1>
-            <h2>FOCUS6</h2>
+?>
+<link rel="stylesheet" href="<?= BASEURL_PUBLIC ?>assets/css/home.css"><?php
+include_once("core/header.php"); ?>
+    <div class="homeContainer">
+        <div class="backgroundImage">
+            <div class="textBox">
+                <h1>SPIEGELSPEL</h1>
+                <h2>FOCUS6</h2>
+            </div>
         </div>
     </div>
-</div>
--- public/info.php
+++ public/info.php
@@ -1,4 +1,31 @@
-<?php 
+<?php
 include_once("core/html.php");
-?><link rel="stylesheet" href="<?=BASEURL_PUBLIC?>assets/css/info.css"><?php
-include_once("core/header.php");?>
+?>
+<link rel="stylesheet" href="<?= BASEURL_PUBLIC ?>assets/css/info.css"><?php
+  include_once("core/header.php"); ?>
+<div class="infoContainer">
+    <div class="videoContainer">
+        <div class="video">
+            <img id="videoImage" src="<?= BASEURL_PUBLIC ?>assets/img/video.png">
+            <p>Meerwaarde</p>
+        </div>
+        <div class="videoText">
+            <h1>DOEL SPIEGELSPEL</h1>
+            <p>Een methodiek om spelenderwijs het teamleren te bevorderen</p>
+        </div>
+    </div>
+    <div class="fotoContainer">
+        <div class="fotoBox">
+            <img class="foto" src="<?= BASEURL_PUBLIC ?>assets/img/foto1.png">
+            <p>Kwaliteit</p>
+        </div>
+        <div class="fotoBox">
+            <img class="foto" src="<?= BASEURL_PUBLIC ?>assets/img/foto2.png">
+            <p>Samenwerken</p>
+        </div>
+        <div class="fotoBox">
+            <img class="foto" src="<?= BASEURL_PUBLIC ?>assets/img/foto2.png">
+            <p>Teamontwikkeling</p>
+        </div>
+    </div>
+</div>
