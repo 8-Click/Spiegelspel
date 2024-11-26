d152ea071baab620859798815e829cfd72ddad7a
marwill206 <laptopmerhino22@hotmail.com>
2024-11-26 15:35:18 +0100

    Implement custom footer with contact details and social icon; update styles for footer layout

--- wp-content/themes/spiegelspelThema/parts/footer.php
+++ wp-content/themes/spiegelspelThema/parts/footer.php
@@ -1,7 +1,26 @@
+<?php
+$args = array(
+    'post_type' => 'attachment',
+    'title' => 'linkd'
+);
+$linkd_id = get_posts($args);
+$linkd_url = wp_get_attachment_url($linkd_id[0]->ID);
+?>
+
+
 <footer>
-        <p>&copy; <?php echo date("Y"); ?> - <?php bloginfo('name'); ?></p>
-        <h1>fjdskf</h1>
-        
-    </footer>
+    <div class="footerContainer">
+        <div class="socialText">
+            <p class="footerP"><strong>Naam contactpersoon:</strong>&nbsp;A.Nijlant</p>
+            <p class="footerP"><strong>Bedrijfsnaam:</strong>&nbsp;Focus6</p>
+            <p class="footerP"><strong>Website:</strong>&nbsp;www.focus6.nl</p>
+            <p class="footerP"><strong>E-mail:</strong>&nbsp;a.nijlant@focus6.nl</p>
+        </div>
+        <div class="linkedInIcon">
+            <img id="icon" src="<?= $linkd_url ?>">
+        </div>
+    </div>
+</footer>
 </body>
+
 </html>
