e7042c9cd562ea08a5e3833e3fc9dcfd4435fd84
marwill206 <laptopmerhino22@hotmail.com>
2024-11-27 09:45:57 +0100

    Add header script enqueue and fix script path; update header styles

--- wp-content/themes/spiegelspelThema/functions.php
+++ wp-content/themes/spiegelspelThema/functions.php
@@ -6,6 +6,11 @@ function mijn_thema_setup() {
 }
 add_action('after_setup_theme', 'mijn_thema_setup');
 
+function mijn_thema_enqueue_scripts() {
+    wp_enqueue_script('header-script', get_template_directory_uri() . '/assets/js/header.js', array(), null, true);
+}
+add_action('wp_enqueue_scripts', 'mijn_thema_enqueue_scripts');
+
 function add_class_to_content_paragraphs($content) {
     // Add a class to each <p> tag
     $content = preg_replace('/<p([^>]+)?>/', '<p$1 class="paragraph">', $content);
