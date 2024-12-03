cb97af0ac7f92bc57d6f3dc4e0fe5ce4e50a216b
marwill206 <laptopmerhino22@hotmail.com>
2024-12-03 13:22:43 +0100

    changes

--- wp-content/themes/spiegelspelThema/parts/header.php
+++ wp-content/themes/spiegelspelThema/parts/header.php
@@ -11,8 +11,8 @@ $image_url = wp_get_attachment_url($logo_id[0]->ID); ?>
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="spiegelspel, A.Nijlant, contact, teamleren, methodiek">
     <meta name="description" content="Website over het spiegelspel van A.Nijlant u kunt u hier ook contact opnemen.">
-
     <title><?php bloginfo('name'); ?></title>
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
     <script src="<?php echo get_template_directory_uri(); ?>/assets/js/header.js"></script>
