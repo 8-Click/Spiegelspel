03dbe7f7cf0c9811c3c763ce3c7e9a26b862a4b5
marwill206 <laptopmerhino22@hotmail.com>
2025-01-30 14:05:07 +0100

    Add backup files and update header with SEO metadata and favicon links

--- wp-content/themes/spiegelspelThema/parts/header.php
+++ wp-content/themes/spiegelspelThema/parts/header.php
@@ -6,7 +6,7 @@ $args = array(
 $logo_id = get_posts($args);
 $image_url = wp_get_attachment_url($logo_id[0]->ID);
 if (empty($image_url)) {
-    $image_url='https://cdn.pixabay.com/photo/2016/09/14/20/50/tooth-1670434_640.png';
+    $image_url = 'https://cdn.pixabay.com/photo/2016/09/14/20/50/tooth-1670434_640.png';
 }
 ?>
 
@@ -14,17 +14,19 @@ if (empty($image_url)) {
 <!DOCTYPE html>
 <html <?php language_attributes();
 
-?>>
+        ?>>
 
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="Website over het spiegelspel van A.Nijlant u kunt u hier ook contact opnemen.">
+    <meta name="keywords" content="Focus6, succes, teamontwikkeling">
+    <meta name="author" content="Focus6 Team">
+    <meta name="robots" content="index, follow">
     <title><?php bloginfo('name'); ?></title>
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
     <script src="<?php echo get_template_directory_uri(); ?>/assets/js/header.js"></script>
     <link rel="icon" href="<?php echo get_template_directory_uri(); ?>assets/img/favicon_io(2)/favicon.ico" type="image/x-icon">
-    
     <link rel="icon" href="<?php echo get_template_directory_uri(); ?>assets/img/favicon_io(2)/favicon.png" type="image/png">
     <link rel="icon" href="<?php echo get_template_directory_uri(); ?>assets/img/favicon_io(2)/favicon.svg" type="image/svg+xml">
 </head>
